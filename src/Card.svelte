a4cb4f75d65299ee9244d7ce2e3c99bd83ed32b3
edudotdev <eduardo.er626@gmail.com>
2022-09-25 22:34:28 -0700

    component card

---
+++ src/Card.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let name, image
+</script>
+
+<div>
+  <img src={image} alt={name} loading='lazy' />
+  <h2>{name}</h2>
+</div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    background: #f5f5f5;
+    border-radius: 10px;
+    padding: 10px;
+  }
+
+  img {
+    border-radius: 10px;
+  }
+
+  h2 {
+    margin: 0;
+  }
+</style>
